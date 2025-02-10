ba3288e4656b225e817368e685a4f8e77c817287
VictorCancino <juanvictorcancino@gmail.com>
2025-02-10 17:52:32 -0600

    Se creó la tabla compras en la base de datos del proyecto usando la sentencia 03 en el fichero 'crear_tablas.sql'

--- Fake_Mart_DB_V1.0/Scripts/Crear_tablas.sql
+++ Fake_Mart_DB_V1.0/Scripts/Crear_tablas.sql
@@ -10,11 +10,24 @@ CREATE TABLE IF NOT EXISTS CLIENTE (
 );
 
 
---Sentencia 01: Crear la tabla PRODUCTO
+--Sentencia 02: Crear la tabla PRODUCTO
 CREATE TABLE IF NOT EXISTS PRODUCTO (
     Id_producto INTEGER PRIMARY KEY AUTOINCREMENT,
     Nombre_producto TEXT NOT NULL,
     Precio_producto REAL NOT NULL,
     Categoria TEXT,
     Distribuidor TEXT
+);
+
+--Sentencia 03: Crear tabla 'COMPRAS' 
+CREATE TABLE IF NOT EXISTS COMPRAS (
+    Id_compra INTEGER PRIMARY KEY AUTOINCREMENT,
+    Id_cliente INTEGER,
+    Id_producto INTEGER,
+    Cantidad INTEGER NOT NULL,
+    Fecha_hora TEXT NOT NULL,
+    Subtotal REAL NOT NULL,
+    Monto_total REAL NOT NULL,
+    FOREIGN KEY (Id_cliente) REFERENCES CLIENTE(Id_cliente),
+    FOREIGN KEY (Id_producto) REFERENCES PRODUCTO(Id_producto)
 );
