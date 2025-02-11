37db32403c6e32270fc4f80b00905a6b9fa4ce11
VictorCancino <juanvictorcancino@gmail.com>
2025-02-11 11:50:18 -0600

    Se insertarón datos en la tabla 'CLIENTES' desde la sentencia 01  en el fichero 'insertar_datos.sql'

---
+++ Fake_Mart_DB_V1.0/Scripts/insertar_datos.sql
@@ -0,0 +1,10 @@
+
+-- Insertar_datos
+--Sentencia 01: Insertar clientes
+INSERT INTO CLIENTE(Nombre_cliente,Direccion_cliente,Telefono, Email)
+VALUES
+      ('Juan Perez Ligonio','Calle mirandas 123', '933-555-4444','juan@fake.com'),
+      ('Maria Lopez Sepulveda','Calle Hojaldras 234', '933-444-5555', 'maria@fake.com'),
+      ('Luisa Sandoval Saravia','Calle Azucenas 345','933-666-3333','luisa@fake.com'),
+      ('Santiago Peregrino Mendez','Calle mirandas 124', '933-345-7890','santiago@fake.com'),
+      ('Armando Casa Palacios', 'Calle Hojaldras 129', '933-888-9999','armando@fake.com');
