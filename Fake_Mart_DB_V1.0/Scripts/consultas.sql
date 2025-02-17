8fddc0884f95c9b6797b7163110a891983b9ded3
VictorCancino <juanvictorcancino@gmail.com>
2025-02-17 11:32:10 -0600

    Se agregó de forma correcta la sentencia 03: 'Obtener las compras realizadas por un clienteespecifico' en el fichero 'consultas.sql'

--- Fake_Mart_DB_V1.0/Scripts/consultas.sql
+++ Fake_Mart_DB_V1.0/Scripts/consultas.sql
@@ -7,3 +7,10 @@ SELECT p.Nombre_producto, p.Precio_producto, i.Cantidad, i.Estatus
 FROM PRODUCTO p 
 JOIN INVENTARIO i on p.Id_producto = i.Id_producto
 WHERE i.Estatus = 'Disponible';
+
+--Sentencia 03: Obtener las compras realizadas por un cliente especifico
+SELECT c.Nombre_cliente, p.Nombre_producto, co.Cantidad, co.Fecha_hora, co.Monto_total
+FROM COMPRAS co 
+JOIN CLIENTE c ON co.Id_cliente = c.Id_cliente
+JOIN PRODUCTO p ON co.Id_producto = p.Id_producto
+WHERE c.Nombre_cliente = 'Juan Perez Ligonio'; 
