d0ea1bfb47622015f46aea217e5a65380b6198e8
VictorCancino <juanvictorcancino@gmail.com>
2025-02-17 11:23:43 -0600

    Se agregó de forma correcta la sentencia 02: 'Obtener todos los productos disponibles' en el fichero 'consultas.sql'

--- Fake_Mart_DB_V1.0/Scripts/consultas.sql
+++ Fake_Mart_DB_V1.0/Scripts/consultas.sql
@@ -1,3 +1,9 @@
 
 --Sentencia 01: Obtener todos los clientes de la Base de Datos
 SELECT * FROM CLIENTE; 
+
+--Sentencia 02: Obtener todos los productos disponibles
+SELECT p.Nombre_producto, p.Precio_producto, i.Cantidad, i.Estatus
+FROM PRODUCTO p 
+JOIN INVENTARIO i on p.Id_producto = i.Id_producto
+WHERE i.Estatus = 'Disponible';
